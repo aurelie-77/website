6cbc47827002df3975dcadf706547b5788d4378b
aurelie-77 <127731135+aurelie-77@users.noreply.github.com>
2023-05-23 21:07:30 +0200

    Add files via upload

---
+++ YoutubeVideo.tsx
@@ -0,0 +1,13 @@
+type VideoProps = {
+    id: string
+}
+
+export const YoutubeVideo : React.FC<VideoProps> = ({id}) => {
+    return (
+        <div>
+            <iframe width="500" height="315"
+            src={"https://www.youtube.com/embed/" + id}
+            title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+        </div>
+    )
+}
