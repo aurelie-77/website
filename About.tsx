04c6c33a7fd74745c7667558e80f977ecea0d9e8
aurelie-77 <127731135+aurelie-77@users.noreply.github.com>
2023-05-23 21:06:26 +0200

    Add files via upload

---
+++ About.tsx
@@ -0,0 +1,13 @@
+
+export const About = () => {
+    return (
+        <section className='mb-5'>
+            <h1 className="text-3xl font-extrabold">Bienvenue</h1>
+            <p className="mt-5">Lorem ipsum dolor sit amet consectetur, 
+            adipisicing elit. Ea eveniet eum assumenda, 
+            saepe ab sunt officiis laudantium odio nisi 
+            molestias.</p>
+        </section>
+
+    )
+}
